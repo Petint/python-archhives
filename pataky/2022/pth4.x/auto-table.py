cf7784181173bc82a7705c2a47907004cd632476
Spaceman2004 <enderdragonkiler2000@gmail.com>
2022-03-07 21:22:31 +0100

    PyCharm CleanUp

--- pataky/2022/pth4.x/auto-table.py
+++ pataky/2022/pth4.x/auto-table.py
@@ -1,15 +1,15 @@
 class Table:
-    def __init__(self,table_data: 'list[list]'):
+    def __init__(self, table_data: 'list[list]'):
         self.tabledata = table_data
 
     def getfirstrow(self) -> str:
         firstrow = '┌─'
-        for __i in range(len(self.tabledata)-1):
+        for __i in range(len(self.tabledata) - 1):
             firstrow += '┬─'
         firstrow += '┐\n'
         return firstrow
 
-    def getrow(self,index: int) -> str:
+    def getrow(self, index: int) -> str:
         row = ''
         for __i in range(len(self.tabledata[index])):
             row += f'│{__i}'
@@ -18,14 +18,14 @@ class Table:
 
     def getlastrow(self) -> str:
         lastrow = '└─'
-        for __i in range(len(self.tabledata)-1):
+        for __i in range(len(self.tabledata) - 1):
             lastrow += '┴─'
         lastrow += '┘\n'
         return lastrow
 
     def getseprow(self) -> str:
         seprow = '├─'
-        for __i in range(len(self.tabledata)-1):
+        for __i in range(len(self.tabledata) - 1):
             seprow += '┼─'
         seprow += '┤\n'
         return seprow
@@ -38,6 +38,7 @@ class Table:
             str_table += self.getseprow()
         str_table += self.getlastrow()
         return str_table
+
 
 if __name__ == "__main__":
     test_data = [
@@ -53,9 +54,6 @@ if __name__ == "__main__":
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-       ]
+    ]
     table1 = Table(test_data)
     print(table1.make())
-
-
-
