fcda34c97461113fdbed5f1058bd90b9c8627700
Spaceman2004 <enderdragonkiler2000@gmail.com>
2022-03-12 12:40:26 +0100

    Mondatcsináló V2

--- mondatcsináló/mondatcsinalo.py
+++ mondatcsináló/mondatcsinalo.py
@@ -1,9 +1,12 @@
 import random
-inputFlie = open("input.txt","r",2048,"utf-8")
-ki = inputFlie.readline().split(",")
-hol = inputFlie.readline().split(",")
-mivel = inputFlie.readline().split(",")
-mit = inputFlie.readline().split(",")
-i = 0
 
-print(f"{ki[random.randint(0,len(ki)-2)]} {hol[random.randint(0,len(hol)-2)]} {mivel[random.randint(0,len(mivel)-2)]} {mit[random.randint(0,len(mit)-2)]}.\n")
+input_file = open("input.txt", "r", 2048, "utf-8")
+words = input_file.read().splitlines()
+input_file.close()
+for i in range(4):
+    words[i] = words[i].split(",")
+ki = words[0][random.randint(0, len(words[0]) - 2)]
+hol = words[1][random.randint(0, len(words[1]) - 2)]
+mivel = words[2][random.randint(0, len(words[2]) - 2)]
+mit = words[3][random.randint(0, len(words[3]) - 2)]
+print(f"{ki} {hol} {mivel} {mit}.\n")
