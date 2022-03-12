f6822f41c55426bc0a048334f0fe7eb848370ffe
Diak <diak@pataky.hu>
2022-03-12 10:37:55 +0100

    Hibakezelés

--- Python 3/p3-5/dolog.py
+++ Python 3/p3-5/dolog.py
@@ -1,5 +1,9 @@
-x = int(input('Addj meg egy számot: '))
-calcd = 2 * x ** 4 + 3 * x ** 3 + 2 * x ** 2 + 7 * x + 4
-print(calcd, x)
-
-print(f"Addj {int(input())} számot.")
+try:
+    x = float(input('Addj meg egy számot: '))
+except ValueError:
+    print("Nem számot adtál meg.")
+else:
+    calcd = 2 * x ** 4 + 3 * x ** 3 + 2 * x ** 2 + 7 * x + 4
+    print(calcd, x)
+finally:
+    print("Program vége.")
