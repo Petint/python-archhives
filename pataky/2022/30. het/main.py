394e5e8bbf1a283f7bb18eca5d8fa29b4cd68785
Petras Balint <petras.balint04@gmail.com>
2022-03-21 07:58:50 +0100

    05-02

--- pataky/2022/30. het/main.py
+++ pataky/2022/30. het/main.py
@@ -9,4 +9,14 @@ path = "koltok.txt"
 koltok_file = open(path, 'rt', encoding="utf-8")
 koltok_file_data = koltok_file.read()
 koltok_file.close()
-print(koltok_file_data)
+# print(koltok_file_data)
+
+"""
+05-02
+Olvasd be a 'magyar-koltok-irok.txt' utf-8 kódolású szövegfájl tartalmát és írd ki a konzolra a költőket."""
+koltok = koltok_file_data.splitlines()
+del koltok_file_data
+# print(koltok)
+for k in koltok:
+    if 'költő' in k.lower():
+        print(k)
