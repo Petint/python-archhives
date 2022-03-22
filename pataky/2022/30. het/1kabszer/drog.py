6d8dd201ccda0a2d7d889a7aed7bfa2733a66597
Spaceman2004 <enderdragonkiler2000@gmail.com>
2022-03-22 18:45:57 +0100

    05-10

--- pataky/2022/30. het/1kabszer/drog.py
+++ pataky/2022/30. het/1kabszer/drog.py
@@ -5,9 +5,14 @@ illegal_drugs = illegal_file.read().splitlines()
 illegal_file.close()
 illegal_data = [illegal_drugs[i].split("\t") for i in range(len(illegal_drugs))]
 
+lots_of_drugs = 0
 for year in illegal_data:
     try:
         if int(year[0]) >= 2016:
             print(f"{year[0]} - {year[1]}")
+        if int(year[1]) > lots_of_drugs:
+            year_of_drugs, lots_of_drugs = int(year[0]), int(year[1])
     except ValueError:
         pass
+
+print(f"{year_of_drugs} évben követték el a legtöbb kábítószeres bűncselekményt : {lots_of_drugs}")
