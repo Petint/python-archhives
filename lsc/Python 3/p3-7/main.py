ec012bb29e2d72ff7534fc7859b6f69f729a821f
Petras Balint <petras.balint04@gmail.com>
2022-03-26 13:54:16 +0100

    Pyramid

--- lsc/Python 3/p3-7/main.py
+++ lsc/Python 3/p3-7/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+bricks = int(input("enter the number of bricks: "))
+height = 0
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+while bricks > 0:
+    if bricks >= height + 1:
+        bricks -= height + 1
+        height += 1
+    else:
+        break
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+res = f"The height of the pyramid {height}, and we have {bricks} bricks left."
+print(res)
