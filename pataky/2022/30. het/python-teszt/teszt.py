45fd9d968ca1fb5a44ff434a87a82efa9a77501a
Petras Balint <petras.balint04@gmail.com>
2022-03-26 09:41:32 +0100

    01-32: File read method

--- pataky/2022/30. het/python-teszt/teszt.py
+++ pataky/2022/30. het/python-teszt/teszt.py
@@ -1,10 +1,13 @@
-# file import
-path = 'feladatok.txt'
-feladatok_file = open(path, 'rt', encoding='utf-8')
-feladatok_raw = feladatok_file.read().splitlines()
-feladatok_file.close()
-feladatok = [f.split(';') for f in feladatok_raw]
-del feladatok_file, feladatok_raw, feladatok[0]
+def file_import(path: str):
+    """Data import from specified file."""
+    _feladatok_file = open(path, 'rt', encoding='utf-8')
+    _feladatok_raw = _feladatok_file.read().splitlines()
+    _feladatok_file.close()
+    _feladatok = [f.split(';') for f in _feladatok_raw]
+    del _feladatok_file, _feladatok_raw, _feladatok[0]
+    return _feladatok
 
+
+feladatok = file_import('feladatok.txt')
 for feladat in feladatok:
     print(feladat)
