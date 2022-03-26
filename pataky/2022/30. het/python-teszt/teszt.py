fe7d0c835c2efcdff37a0df14b339787d064087a
Petras Balint <petras.balint04@gmail.com>
2022-03-26 09:57:12 +0100

    01-32: Ask DU

--- pataky/2022/30. het/python-teszt/teszt.py
+++ pataky/2022/30. het/python-teszt/teszt.py
@@ -8,6 +8,21 @@ def file_import(path: str):
     return _feladatok
 
 
+def ask(question: 'list[str]'):
+    answer = question[-1]
+    print(f"""
+Az alabbi muveletnek mi az eredmenye?
+{question[0]}
+1) {question[1]}
+2) {question[2]}
+3) {question[3]}
+    """)
+    dear_user = input('Melyik a helyes valasz (1, 2, 3): ')
+    return dear_user == answer, answer
+
+
 feladatok = file_import('feladatok.txt')
+print("Python muveletek - TESZT")
 for feladat in feladatok:
-    print(feladat)
+    answer_bool, answer = ask(feladat)
+    print('Helyes valasz') if answer_bool else print(f'Helytelen valasz. A helyes valasz: {answer}')
