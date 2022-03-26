f077656c5334316a4172280c689dfe4cdb8dfc8f
Petras Balint <petras.balint04@gmail.com>
2022-03-26 14:06:49 +0100

    De morgan

--- lsc/Python 3/p3-7/main.py
+++ lsc/Python 3/p3-7/main.py
@@ -1,12 +1,26 @@
-bricks = int(input("enter the number of bricks: "))
-height = 0
+def pyramid():
+    bricks = int(input("enter the number of bricks: "))
+    height = 0
 
-while bricks > 0:
-    if bricks >= height + 1:
-        bricks -= height + 1
-        height += 1
-    else:
-        break
+    while bricks > 0:
+        if bricks >= height + 1:
+            bricks -= height + 1
+            height += 1
+        else:
+            break
 
-res = f"The height of the pyramid {height}, and we have {bricks} bricks left."
-print(res)
+    res = f"The height of the pyramid {height}, and we have {bricks} bricks left."
+    print(res)
+
+
+def chi9gu5():
+    """De Morgan identity"""
+    p = True
+    q = False
+    print(not(p and q) == (not p or not q))
+    print(not (p or q) == (not p and not q))
+
+
+if __name__ == '__main__':
+    pyramid()
+    chi9gu5()
