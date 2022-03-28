bab7050976e4daf3b4e9452b6a798e0768155d5c
Petras Balint Gyorgy <2020a_petras_ba@inf.local>
2022-03-28 11:51:29 +0200

    e

--- pataky/2022/31. hét/03-07.py
+++ pataky/2022/31. hét/03-07.py
@@ -1,5 +1,10 @@
+def ask_du():
+    pass
+
+
 def menu():
-    print("e")
+    print('A program bekér egy szöveget és a választásod alapján kiír ezt-azt a szövegről. Üres szövegre leáll.')
+    du = ask_du()
 
 if __name__ == "__main__":
     menu()
