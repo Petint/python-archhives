6278e3377c60fb034acaa146e81781771a795939
Petras Balint <petras.balint04@gmail.com>
2022-03-31 13:46:17 +0200

    02-10

--- pataky/2022/31. hét/2-10.py
+++ pataky/2022/31. hét/2-10.py
@@ -4,7 +4,10 @@ secret = randint(1, 5)
 tipp = 0
 while tipp is not secret:
     try:
-        tipp = int(input('Tippelj 1-5 kozott: '))
+        du = input('Tippelj 1-5 kozott: ')
+        if du == '':
+            exit('Ures szoveget atdal meg.')
+        tipp = int(du)
     except ValueError:
         print('Nem szamot adtal meg!')
     if tipp is not secret:
