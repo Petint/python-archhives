5225138f9f5a515cc6156c19b0b7e90b11324950
Diak <dingus>
2022-04-02 10:24:56 +0200

    Still doesn't work...

--- lsc/Python 3/p3-8/main.py
+++ lsc/Python 3/p3-8/main.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 blue = (0, 0, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
@@ -7,16 +8,15 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 x1 = 200
 y1 = 100
-
 x1_change = 0
 y1_change = 0
 dis = pygame.display((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake')
-pygame.draw.rect(self.dis, self.blue, [200, 150, 10, 10])
+pygame.draw.rect(dis, blue, [200, 150, 10, 10])
 game_over = False
 while not game_over:
-    for event in pygame:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = False
 pygame.quit()
