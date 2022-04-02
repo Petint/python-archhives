177f1c2016d1eb7034837dfd696942e9e850b22d
Diak <dingus>
2022-04-02 10:54:03 +0200

    Q.Q

--- lsc/Python 3/p3-8/main.py
+++ lsc/Python 3/p3-8/main.py
@@ -8,16 +8,11 @@ green = (0, 255, 0)
 red = (255, 0, 0)
 white = (255, 255, 255)
 black = (0, 0, 0)
-x1 = 200
-y1 = 100
-x1_change = 0
-y1_change = 0
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake')
 pygame.draw.rect(dis, blue, [200, 150, 10, 10])
 pygame.display.update()
-game_over = False
 snake_block = 10
 font_stlye = pygame.font.SysFont("Arial", 50)
 # pygame.time.Clock.__init__()
@@ -28,6 +23,11 @@ def massage(msg: str, color: 'tuple(int, int, int)'):
 
 
 def game_loop():
+    game_over = False
+    x1 = 200
+    y1 = 100
+    x1_change = 0
+    y1_change = 0
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
