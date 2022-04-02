65d77bf006a68d8ece0c5578ff1a24d7408b6828
Diak <dingus>
2022-04-02 10:35:42 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- lsc/Python 3/p3-8/main.py
+++ lsc/Python 3/p3-8/main.py
@@ -10,7 +10,7 @@ x1 = 200
 y1 = 100
 x1_change = 0
 y1_change = 0
-dis = pygame.display((400, 300))
+dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake')
 pygame.draw.rect(dis, blue, [200, 150, 10, 10])
@@ -20,5 +20,20 @@ while not game_over:
     for event in pygame:
         if event.type == pygame.QUIT:
             game_over = False
+        if event.type == pygame.event:
+            if pygame.key == "a":
+                x1_change = -1
+            if pygame.key = "d"
+                x1_change = 1
+            if pygame == w:
+                y1_change = -1
+            if game_over == 's':
+                y1_change = 1
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(black)
+        pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+        pygame.clock.tick(30)
 pygame.quit()
+quit()
 
