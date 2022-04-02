3177621557950fb6ecb89ff18a154081005ea28f
Diak <dingus>
2022-04-02 10:27:21 +0200

    Wronk baranch rys

--- lsc/Python 3/p3-8/main.py
+++ lsc/Python 3/p3-8/main.py
@@ -1,6 +1,6 @@
 import pygame
 
-
+pygame.init()
 blue = (0, 0, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
@@ -14,9 +14,10 @@ dis = pygame.display((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake')
 pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+pygame.display.update()
 game_over = False
 while not game_over:
-    for event in pygame.event.get():
+    for event in pygame:
         if event.type == pygame.QUIT:
             game_over = False
 pygame.quit()
