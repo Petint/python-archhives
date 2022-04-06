9f1f35999b4919d2cf709c2bc801f864efd74140
Petint <petras.balint04@gmail.com>
2022-04-06 22:45:27 +0200

    Now to make it look good

--- pataky/2022/31. hét/01-02.py
+++ pataky/2022/31. hét/01-02.py
@@ -8,10 +8,25 @@ else:
 """
 import tkinter
 
+
+def feldolgoz():
+    nums = int(input_box1.get()), int(input_box2.get())
+    if nums[0] == nums[1]:
+        text = "A két szám egyenlő."
+    else:
+        text = str(max(nums))
+    result = tkinter.Label(window, text=text)
+    result.pack()
+
+
 window = tkinter.Tk()
 window.title('01-02')
 feladat = tkinter.Label(window, text="Két beolvasott egész számról állapítjuki meg, hogy melyik a nagyobb.")
-input_lable = tkinter.Label(window, text="Adj meg egy egész számot")
+input_box1 = tkinter.Entry(window)
+input_box2 = tkinter.Entry(window)
+button = tkinter.Button(window, text="Feldolgozás", command=feldolgoz)
 feladat.pack()
-input_lable.pack()
+input_box1.pack()
+input_box2.pack()
+button.pack()
 window.mainloop()
