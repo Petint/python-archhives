d540be0f84db78b79a39dcfb264e82fd8a4a29a1
Petint <petras.balint04@gmail.com>
2022-04-06 22:06:08 +0200

    Basic stuff

--- pataky/2022/31. hét/01-02.py
+++ pataky/2022/31. hét/01-02.py
@@ -1,6 +1,15 @@
+"""
 print("Két beolvasott egész számról állapítjuki meg, hogy melyik a nagyobb.")
 nums = int(input("Adj meg egy egész számot: ")), int(input("Adj meg egy egész számot: "))
 if nums[0] == nums[1]:
     print("A két szám egyenlő.")
 else:
     print(max(nums))
+"""
+import tkinter
+
+window = tkinter.Tk()
+window.title('01-02')
+feladat = tkinter.Label(window, text="Két beolvasott egész számról állapítjuki meg, hogy melyik a nagyobb.")
+feladat.pack()
+window.mainloop()
