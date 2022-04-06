60580bd4be71ab49728e818923b8ffc36548224d
Petint <petras.balint04@gmail.com>
2022-04-06 22:25:52 +0200

    Some of that...

--- pataky/2022/31. hét/01-02.py
+++ pataky/2022/31. hét/01-02.py
@@ -11,5 +11,7 @@ import tkinter
 window = tkinter.Tk()
 window.title('01-02')
 feladat = tkinter.Label(window, text="Két beolvasott egész számról állapítjuki meg, hogy melyik a nagyobb.")
+input_lable = tkinter.Label(window, text="Adj meg egy egész számot")
 feladat.pack()
+input_lable.pack()
 window.mainloop()
