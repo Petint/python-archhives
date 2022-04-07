9afd3dce4f234b2f3764a2c37f516741f8507725
Petras Balint <petras.balint04@gmail.com>
2022-04-07 13:27:35 +0200

    03-02 pt1

--- pataky/2022/31. hét/03-xg.py
+++ pataky/2022/31. hét/03-xg.py
@@ -24,11 +24,24 @@ def paratlan_check():
     else:
         rtxt = 'A szam paros.'
     eredmeny = tkinter.Label(paratlanablak, text=rtxt)
-    eredmeny.pack
+    eredmeny.pack()
+
+
+def nagyobbdupla():
+    nagyobbablak = tkinter.Toplevel(window)
+    instrukcio = tkinter.Message(paratlanablak, text='Addj be ket szamot es kiadom a nagyobb dulajat.')
+    input_box1 = tkinter.Entry(paratlanablak)
+    the_button = tkinter.Button(paratlanablak, text='szamol')
+    instrukcio.pack()
+    input_box1.pack()
+    the_button.pack()
+    nagyobbablak.mainloop()
 
 
 window = tkinter.Tk()
 window.title('Fuggvenyes feladatok')
 paratlan = tkinter.Button(window, text='03-01', command=paratlanfeladat)
+dupla = tkinter.Button(window, text='03-02', command=nagyobbdupla)
 paratlan.grid(row=1, column=1)
+dupla.grid(row=1, column=2)
 window.mainloop()
