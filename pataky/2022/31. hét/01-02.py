ddfea0230ad7f0653b0adbfc2f587145cacf0523
Petint <petras.balint04@gmail.com>
2022-04-09 16:01:04 +0200

    ugu

--- pataky/2022/31. hét/01-02.py
+++ pataky/2022/31. hét/01-02.py
@@ -16,17 +16,20 @@ def feldolgoz():
     else:
         text = str(max(nums))
     result = tkinter.Label(window, text=text)
-    result.pack()
+    result.grid(row=2, column=2)
 
 
 window = tkinter.Tk()
-window.title('01-02')
-feladat = tkinter.Label(window, text="Két beolvasott egész számról állapítjuki meg, hogy melyik a nagyobb.")
+window.title('01-02 Két beolvasott egész számról állapítjuki meg, hogy melyik a nagyobb.')
+
 input_box1 = tkinter.Entry(window)
 input_box2 = tkinter.Entry(window)
 button = tkinter.Button(window, text="Feldolgozás", command=feldolgoz)
-feladat.pack()
-input_box1.pack()
-input_box2.pack()
-button.pack()
+for i in range(4):
+    window.columnconfigure(i, pad=15)
+    window.rowconfigure(i, pad=10)
+input_box1.grid(row=1, column=1)
+button.grid(row=1, column=2)
+input_box2.grid(row=1, column=3)
+
 window.mainloop()
