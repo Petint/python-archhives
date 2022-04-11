5b032a5c190f737a5c9f4b08b52daac5765626e6
2020a_petras_ba <asd@pataky.hu>
2022-04-11 11:50:21 +0200

    törtek kiírása

--- pataky/2022/aprilis11/tort.py
+++ pataky/2022/aprilis11/tort.py
@@ -4,4 +4,8 @@ from random import randint
 nums = [randint(10, 100) for _ in range(4)]
 tort1 = nums[0], nums[1]
 tort2 = nums[2], nums[3]
-print(tort1, tort2)
+del nums
+print(f"""
+x = {tort1[0]}/{tort1[1]}
+y = {tort2[0]}/{tort2[1]}
+""")
