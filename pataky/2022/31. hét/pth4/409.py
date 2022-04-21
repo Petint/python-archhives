b20fed6a92b151007e7d8d9055f17ed9780c7b5b
Petras Balint <petras.balint04@gmail.com>
2022-04-21 14:05:18 +0200

    409

---
+++ pataky/2022/31. hét/pth4/409.py
@@ -0,0 +1,28 @@
+dataList = """Név\tÉletkor\tVáros
+Agárdi Pál\t26\tBudapest
+Pálosi Richárd\t23\tBudapest
+Budai Máté\t19\tDebrecen
+Karácsony Antal\t20\tBudapest
+Román Sarolta\t24\tBudapest
+Virág Bertalan\t22\tPécs
+Varga Imre\t18\tBudapest
+Tóth Sándor\t22\tDebrecen
+Nagy Ibolya\t23\tPécs
+Horváth Ferenc\t17\tBudapest
+Balogh Edina\t26\tBudapest
+Németh Kamilla\t19\tDebrecen
+Fekete Géza\t18\tPécs
+Kovács Péter\t27\tBudapest
+Kiss Tibor\t20\tDebrecen
+Szabó Erzsébet\t21\tBudapest
+Szilágyi Ede\t18\tPécs
+Aradi Márta\t27\tPécs
+Piros Adél\t29\tDebrecen
+Bíró Zsolt\t16\tBudapest
+Szabados Attila\t25\tDebrecen
+"""
+
+for ppl in dataList.splitlines()[1:]:
+    nev, kor, varos = ppl.split('\t')
+    # print(nev, kor, varos)
+
