130cefd597ab9932a20e318139c64ca2d27ff02b
Petint <petras.balint04@gmail.com>
2022-04-24 21:02:31 +0200

    01-01

---
+++ pataky/python.tanit.hu/snipets.py
@@ -0,0 +1,13 @@
+# Hibakezeléses számbekérés
+try:
+    du = int(input("Adj meg egy számot: "))
+except ValueError:
+    print("Nem számot adtál meg")
+else:
+    pass
+# isnumeric számbekérés
+du = input("Adj meg egy számot: ")
+if du.isnumeric():
+    pass
+else:
+    pass
