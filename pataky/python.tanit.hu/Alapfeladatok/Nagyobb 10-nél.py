b55b3dbf8b4edf1ba7f7dc0e0cffc1fb3d413d89
Petint <petras.balint04@gmail.com>
2022-04-24 22:23:51 +0200

    01-05 Nagyobb 10-nél

--- pataky/python.tanit.hu/Alapfeladatok/Nagyobb 10-nél.py
+++ pataky/python.tanit.hu/Alapfeladatok/Nagyobb 10-nél.py
@@ -4,3 +4,10 @@ Feladat leírás:
 Olvass be egy egész számot. Írd ki ha nagyobb tíznél: "Nagy", különben azt, hogy "Kicsi".
 Biztosra vehetjük, hogy egész számot adunk meg.
 """
+print("Megálapítjuk egy számról hogy nagyobb-e 10-nél.")
+du = int(input("Adj meg egy számot: "))
+
+if du > 10:
+    print("Nagy")
+else:
+    print("Kicsi")
