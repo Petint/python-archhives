e7331ab0fd8fdfa76b07c66d4debf9f6471b8d4a
Petint <petras.balint04@gmail.com>
2022-04-24 21:58:04 +0200

    01-05 Nagyobb 10-nél

---
+++ pataky/python.tanit.hu/Alapfeladatok/Nagyobb 10-nél.py
@@ -0,0 +1,6 @@
+"""
+01-05 Nagyobb 10-nél
+Feladat leírás:
+Olvass be egy egész számot. Írd ki ha nagyobb tíznél: "Nagy", különben azt, hogy "Kicsi".
+Biztosra vehetjük, hogy egész számot adunk meg.
+"""
