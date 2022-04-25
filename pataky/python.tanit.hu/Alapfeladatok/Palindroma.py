de02b47cfcad1d13f09da9b4ffc1e845b36ff615
Petint <petras.balint04@gmail.com>
2022-04-25 22:54:59 +0200

    01-30 Palindroma

--- pataky/python.tanit.hu/Alapfeladatok/Palindroma.py
+++ pataky/python.tanit.hu/Alapfeladatok/Palindroma.py
@@ -6,5 +6,10 @@ Palindroma az a szöveg, amely előröl és visszafele is ugyanaz. A szóközök
 """
 print("Egy sövegről megállapítjuk hogy palidrma-e.")
 du = input("Adj meg egy szöveget: ").lower()
-
-
+is_palidrome = False
+if len(du) > 1:
+    for x in range(len(du)):
+        is_palidrome = du[x] == du[-x-1]
+else:
+    is_palidrome = False
+print("Ez egy palindróma.") if is_palidrome else print("Ez nem egy palindróma.")
