b2762d66d1a3ba8231f2e46d9de40a23691eea5c
Petint <petras.balint04@gmail.com>
2022-04-25 22:47:42 +0200

    01-30 Palindroma

--- pataky/python.tanit.hu/Alapfeladatok/Palindroma.py
+++ pataky/python.tanit.hu/Alapfeladatok/Palindroma.py
@@ -5,4 +5,6 @@ Egy beolvasott szövegről állapítsuk meg, hogy palindroma-e.
 Palindroma az a szöveg, amely előröl és visszafele is ugyanaz. A szóközök és a kis és nagy betűk nem számítanak.
 """
 print("Egy sövegről megállapítjuk hogy palidrma-e.")
-du = input("Adj meg egy szöveget: ")
+du = input("Adj meg egy szöveget: ").lower()
+
+
