8b15f8371857d5bebe5183aa185ace0651ef22a5
Diak <diak@bbc.co.uk>
2022-04-30 09:53:48 +0200

    p3-11/1

--- lsc/Python 3/p3-11/main.py
+++ lsc/Python 3/p3-11/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def new_file():
+    my_file = open("sheet.txt", "w+")
+    my_file.write("So it goes - Vonnegut\n")
+    my_file.seek(0)
+    print(my_file.read())
+    my_file.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+new_file()
