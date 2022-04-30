a34a3601a4b37f8a5d4c33896509ac6731a78d4d
Diak <diak@bbc.co.uk>
2022-04-30 10:04:48 +0200

    p3-11/3

--- lsc/Python 3/p3-11/main.py
+++ lsc/Python 3/p3-11/main.py
@@ -1,17 +1,25 @@
-"""def new_file():
+def file_read():
+    my_file = open("sheet.txt", "r")
+    print(my_file.read())
+    my_file.close()
+
+
+def new_file():
     my_file = open("sheet.txt", "w+")
     my_file.write("So it goes - Vonnegut\n")
-    my_file.seek(0)
+    """my_file.seek(0)
     print(my_file.read())
     my_file.close()"""
 
 
 def file_append():
     my_file = open("sheet.txt", "a+")
-    my_file.write("So it stops - Unknown/me\n")
-    my_file.seek(0)
+    my_file.write("If you try you can fail.\n")
+    """my_file.seek(0)
     print(my_file.read())
-    my_file.close()
+    my_file.close()"""
 
 
-# new_file()
+new_file()
+file_append()
+file_read()
