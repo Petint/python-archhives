96d57f889b40ece856c473733e80e6d0a5b3fc03
Diak <diak@bbc.co.uk>
2022-05-02 11:39:44 +0200

    kosin koltok

--- pataky/2022/majus/osoi.py
+++ pataky/2022/majus/osoi.py
@@ -1,7 +1,10 @@
-print('kiírjuk a file tartalmát.')
+print('kiírjuk a költőket tartalmát.')
 path = "magyar-koltok-irok.txt"
 file = open(path, "r", encoding="utf-8")
 fdata = file.read()
 # print(fdata)
-
+fdata = fdata.splitlines()
+for ember in fdata:
+    if "költő" in ember:
+        print(ember)
 f.colse()
