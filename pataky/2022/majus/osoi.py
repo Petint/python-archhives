3b35960cc9eb31245aebf325bdb7866531b2bce0
Diak <diak@bbc.co.uk>
2022-05-02 11:57:39 +0200

    ebÉd

--- pataky/2022/majus/osoi.py
+++ pataky/2022/majus/osoi.py
@@ -3,7 +3,10 @@ file = open(path, "r", encoding="utf-8")
 fdata = file.read()
 # print(fdata)
 fdata = fdata.splitlines()
+irok_file = open("irokfile.tyt", "wt", encoding="utf-8")
 for ember in fdata:
     if "költő" in ember and "író" in ember:
         print(ember)
+        irok_file.write(ember)
 file.close()
+irok_file.close()
