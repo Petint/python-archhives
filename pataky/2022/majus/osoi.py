fe0deec30a5f67cb3c10bddd0b70720e4a975dc1
Diak <diak@bbc.co.uk>
2022-05-02 11:45:23 +0200

    költő és író hfumgzugfugf

--- pataky/2022/majus/osoi.py
+++ pataky/2022/majus/osoi.py
@@ -1,10 +1,9 @@
-print('kiírjuk a költőket tartalmát.')
 path = "magyar-koltok-irok.txt"
 file = open(path, "r", encoding="utf-8")
 fdata = file.read()
 # print(fdata)
 fdata = fdata.splitlines()
 for ember in fdata:
-    if "költő" in ember:
+    if "költő" in ember and "író" in ember:
         print(ember)
 file.close()
