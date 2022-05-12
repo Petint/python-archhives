646ecf0938fc0e1b09ec823ac4c1dc37ee5326a0
Petint <petras.balint04@gmail.com>
2022-05-12 14:25:24 +0200

    closed sewer

--- pataky/2022/31. hét/pth4/verseny.py
+++ pataky/2022/31. hét/pth4/verseny.py
@@ -1,4 +1,7 @@
 path = "eredmeny.txt"
-file = open(path,'r')
+file = open(path, 'r')
 text = file.read()
 file.close()
+lista = text.splitlines()
+arraym = [l.split(';') for l in lista]
+print(arraym)
