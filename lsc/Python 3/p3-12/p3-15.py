9def22a22a1c3ea96e00d26a6cc28e0cb6f724ab
Petint <petras.balint04@gmail.com>
2022-05-21 10:49:57 +0200

    tuple shish kebab

--- lsc/Python 3/p3-12/p3-15.py
+++ lsc/Python 3/p3-12/p3-15.py
@@ -1,7 +1,24 @@
 gfg = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-fgf = []
-for i in gfg:
-    for j in i:
-        fgf.append(j)
+
+
+def matrix_to_list(gfg: 'list[list]') -> list:
+    fgf = []
+    for i in gfg:
+        for j in i:
+            fgf.append(j)
+    return fgf
+
+
+def remove_kebab(tp: tuple, val):
+    tp = list(tp)
+    tp.remove(val)
+    return tuple(tp)
+
+
+ggg = matrix_to_list(gfg)
 print(gfg)
-print(fgf)
+print(ggg)
+ggfg = tuple(ggg)
+print(remove_kebab(ggfg, 5))
+
+
