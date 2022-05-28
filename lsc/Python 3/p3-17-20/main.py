ce2671c23e909d0e91487c429edb2917dfe53b17
Petint <petras.balint04@gmail.com>
2022-05-28 10:03:09 +0200

    420 Cannabis

---
+++ lsc/Python 3/p3-17-20/main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+my_vec = pygame.math.Vector2
+HEIGHT = 420
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+framsper = pygame.time.Clock()
+disp_srf = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption('Platformer')
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30, 40))
+        self.surf.fill((128, 255, 40))
+        self.rect = self.surf.get_rect(center=(10, 420))
