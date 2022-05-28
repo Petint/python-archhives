606262b7e6450a65faa7b27e4765d76e9bb84152
Petint <petras.balint04@gmail.com>
2022-05-28 10:12:40 +0200

    from pygame import *

--- lsc/Python 3/p3-17-20/main.py
+++ lsc/Python 3/p3-17-20/main.py
@@ -1,29 +1,39 @@
-import pygame
+from pygame import *
 
-pygame.init()
-my_vec = pygame.math.Vector2
+init()
+my_vec = math.Vector2
 HEIGHT = 420
 WIDTH = 400
 ACC = 0.5
 FRIC = -0.12
 FPS = 60
 
-framsper = pygame.time.Clock()
-disp_srf = pygame.display.set_mode(WIDTH, HEIGHT)
-pygame.display.set_caption('Platformer')
+framsper = time.Clock()
+disp_srf = display.set_mode(WIDTH, HEIGHT)
+display.set_caption('Platformer')
 
 
-class Player(pygame.sprite.Sprite):
+class Player(sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((30, 40))
+        self.surf = Surface((30, 40))
         self.surf.fill((128, 255, 40))
         self.rect = self.surf.get_rect(center=(10, 420))
 
 
-class Platform(pygame.sprite.Sprite):
+class Platform(sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf = Surface((WIDTH, 20))
         self.surf.fill((0xff, 0, 0))
         self.rect = self.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
+
+
+PT1 = Platform()  # Platform
+PL1 = Player()    # Player
+
+all_sprites = sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(PL1)
+
+platforms = sprite.Group()
