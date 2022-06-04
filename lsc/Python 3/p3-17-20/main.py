c1119739853fd7ed5f1b236d68db02674314fa70
Petint <petras.balint04@gmail.com>
2022-06-04 10:17:56 +0200

    updated main.py

--- lsc/Python 3/p3-17-20/main.py
+++ lsc/Python 3/p3-17-20/main.py
@@ -1,22 +1,23 @@
-import pygame
+from pygame import *
+import random
 
-pygame.init()
-myVector = pygame.math.Vector2
+init()
+myVector = math.Vector2
 HEIGHT = 450
 WIDTH = 400
 ACC = 0.5
 FRIC = -0.12
 FPS = 60
 
-FramePerSec = pygame.time.Clock()
-displaySurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Platformer")
+FramePerSec = time.Clock()
+displaySurface = display.set_mode((WIDTH, HEIGHT))
+display.set_caption("Platformer")
 
 
-class Player(pygame.sprite.Sprite):
+class Player(sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((30, 30))
+        self.surf = Surface((30, 30))
         self.surf.fill((128, 255, 40))
         self.rect = self.surf.get_rect(center=(10, 420))
 
@@ -26,10 +27,10 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
         self.acc = myVector(0, 0.5)
-        pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[pygame.K_LEFT]:
+        pressed_keys = key.get_pressed()
+        if pressed_keys[K_LEFT]:
             self.acc.x = -ACC
-        if pressed_keys[pygame.K_RIGHT]:
+        if pressed_keys[K_RIGHT]:
             self.acc.x = ACC
         self.acc.x += self.vel.x * FRIC
         self.vel += self.acc
@@ -40,18 +41,23 @@ class Player(pygame.sprite.Sprite):
             self.pos.x = WIDTH
 
     def update(self):
-        hits = pygame.sprite.spritecollide(P1, platforms, False)
-        if hits:
+        hits = sprite.spritecollide(P1, platforms, False)
+        if hits and self.vel.y > 0:
             self.pos.y = hits[0].rect.top + 1
             self.vel.y = 0
 
         self.rect.midbottom = self.pos
+        
+    def jump(self):
+        hits = sprite.spritecollide(P1, platforms, False)
+        if hits:
+            self.vel.y -= 15
 
 
-class Platform(pygame.sprite.Sprite):
+class Platform(sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf = Surface((WIDTH, 20))
         self.surf.fill((255, 0, 0))
         self.rect = self.surf.get_rect(center=(WIDTH/2, HEIGHT-10))
 
@@ -59,22 +65,30 @@ class Platform(pygame.sprite.Sprite):
 PT1 = Platform()
 P1 = Player()
 
-all_sprites = pygame.sprite.Group()
+all_sprites = sprite.Group()
 all_sprites.add(PT1)
 all_sprites.add(P1)
 
-platforms = pygame.sprite.Group()
+platforms = sprite.Group()
 platforms.add(PT1)
 
+for _ in range(6):
+    pl = Platform()
+    platforms.add(pl)
+    all_sprites.add(pl)
+
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for evnt in event.get():
+        if evnt.type == QUIT:
+            quit()
+        if evnt.type == KEYDOWN:
+            if evnt.key == K_SPACE:
+                P1.jump()
     displaySurface.fill((0, 0, 0))
 
     for entity in all_sprites:
         displaySurface.blit(entity.surf, entity.rect)
     P1.move()
     P1.update()
-    pygame.display.update()
+    display.update()
     FramePerSec.tick(FPS)
