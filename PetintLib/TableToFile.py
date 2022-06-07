4fc8719483566a3556be7c12e2a02b66f2427741
Petint <petras.balint04@gmail.com>
2022-06-07 18:14:07 +0200

    Writing table to file: TableToFile.py

--- PetintLib/TableToFile.py
+++ PetintLib/TableToFile.py
@@ -3,5 +3,5 @@ from autotable import Table
 size = 25
 test_data = [[x * size + y for y in range(size)] for x in range(size)]
 table1 = Table(test_data, length=5, align='c')
-with open("table.txt", "w") as f:
+with open("table.txt", "w", encoding="utf-8") as f:
     f.write(table1.make())
