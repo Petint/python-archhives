94d980f0da5016bfcab742251da7998355a6bb95
Petint <petras.balint04@gmail.com>
2022-06-07 19:11:06 +0200

    raw json read III

--- PetintLib/tests/json interpreter.py
+++ PetintLib/tests/json interpreter.py
@@ -1,6 +1,8 @@
 import json
 # from autotable import Table
 
-jsonfile = open("427520.json.json", 'rt')
+jsonfile = open("427520.json", 'rt')
 jsondata = json.load(jsonfile)
+jsonfile.close()
 print(jsondata)
+
