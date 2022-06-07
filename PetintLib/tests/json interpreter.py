ebee2d42716f31ad1d7301c5c1c263513c1cd071
Petint <petras.balint04@gmail.com>
2022-06-07 19:12:12 +0200

    raw json Table I

--- PetintLib/tests/json interpreter.py
+++ PetintLib/tests/json interpreter.py
@@ -1,8 +1,10 @@
 import json
-# from autotable import Table
+from autotable import Table
 
 jsonfile = open("427520.json", 'rt')
 jsondata = json.load(jsonfile)
 jsonfile.close()
 print(jsondata)
 
+json_table = Table(jsondata)
+print(json_table.make())
