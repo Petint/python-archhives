126d2c6fbb41e37e2a863670b58400d70858acb1
Petint <petras.balint04@gmail.com>
2022-06-23 22:01:50 +0200

    Seriália 4 xonxoff

--- seriália/main.py
+++ seriália/main.py
@@ -7,7 +7,7 @@ import serial
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ser = serial.Serial('COM1', 9600)
+    ser = serial.Serial('COM1', 9600, xonxoff=1)
     ser.write(bytes("Add meg, hogy mi a neved: ", encoding='utf-8'))
     line = ser.readline()
     ser.write(bytes(f'\n\rHi, {line.decode()} \r', encoding='utf-8'))
