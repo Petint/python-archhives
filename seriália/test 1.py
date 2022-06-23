385410cbb73a4de0aba02ea0c56c11a57dea42c8
Petint <petras.balint04@gmail.com>
2022-06-23 22:05:13 +0200

    Seriália 5 print(ser.name)

--- seriália/test 1.py
+++ seriália/test 1.py
@@ -1,6 +1,6 @@
 import serial
 ser = serial.Serial('com1')  # open serial port
-# print(ser.name)         # check which port was really used
+print(ser.name)         # check which port was really used
 ser.write(b'hello')     # write a string
 line = ser.readline()   # read a '\n' terminated line
 print(line.decode())
