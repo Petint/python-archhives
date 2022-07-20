61573504e4d796987e9fd682665fc6d1165a3553
Petint <petras.balint04@gmail.com>
2022-07-20 15:31:34 +0200

    music to image

--- seriália/Pi to jpg/pipng.py
+++ seriália/Pi to jpg/pipng.py
@@ -1,11 +1,11 @@
 from PIL import Image
 
 # Get pi
-with open('pi.txt', 'rt') as pifile:
-    piBytes = bytes(pifile.read(), pifile.encoding)
+with open('portal_procedural_jiggle_bone.txt', 'rb') as pifile:
+    piBytes = pifile.read()
 # Convert pi to image
 size = 892
 im = Image.frombytes("1", (size, size), piBytes)
 print(im.format, im.size, im.mode)
 # Save image to file
-im.save('piimg.png', 'PNG')
+im.save('portal_procedural_jiggle_bone.png', 'PNG')
