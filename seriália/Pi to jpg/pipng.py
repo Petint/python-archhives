0b9f71ebea4f029341184debd748fdda2d1c30e0
Petint <petras.balint04@gmail.com>
2022-07-20 14:48:29 +0200

    int pi to byte to image

--- seriália/Pi to jpg/pipng.py
+++ seriália/Pi to jpg/pipng.py
@@ -3,3 +3,6 @@ from PIL import Image
 with open('pi.txt', 'rt') as pifile:
     pi = int(pifile.read())
 
+piBytes = pi.to_bytes(2, 'big')
+im = Image.open(piBytes)
+print(im.format, im.size, im.mode)
