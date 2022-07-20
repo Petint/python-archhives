439f3562894c89f41f16f2c6c1140b4060c12195
Petint <petras.balint04@gmail.com>
2022-07-20 15:04:00 +0200

    Save image

--- seriália/Pi to jpg/pipng.py
+++ seriália/Pi to jpg/pipng.py
@@ -1,8 +1,10 @@
 from PIL import Image
-from math import sqrt
 
+# Get pi
 with open('pi.txt', 'rt') as pifile:
     piBytes = bytes(pifile.read(), pifile.encoding)
-
+# Convert pi to image
 im = Image.frombytes("RGB", (100, 100), piBytes)
 print(im.format, im.size, im.mode)
+# Save image to file
+im.save('piimg.jpg', 'JPEG')
