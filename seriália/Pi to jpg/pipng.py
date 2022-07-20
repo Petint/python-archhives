59f62b8c1e42a130469f44a07342638435b8274e
Petint <petras.balint04@gmail.com>
2022-07-20 15:12:44 +0200

    Small tweaks, image

--- seriália/Pi to jpg/pipng.py
+++ seriália/Pi to jpg/pipng.py
@@ -4,7 +4,8 @@ from PIL import Image
 with open('pi.txt', 'rt') as pifile:
     piBytes = bytes(pifile.read(), pifile.encoding)
 # Convert pi to image
-im = Image.frombytes("RGB", (100, 100), piBytes)
+size = 892
+im = Image.frombytes("1", (size, size), piBytes)
 print(im.format, im.size, im.mode)
 # Save image to file
-im.save('piimg.jpg', 'JPEG')
+im.save('piimg.png', 'PNG')
