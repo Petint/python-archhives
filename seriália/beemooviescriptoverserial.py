98eb5fc885db2cb83e01860eda33df73c53e7f35
Petrás Bálint <petras.balint04@gmail.com>
2022-11-12 23:14:43 +0100

    BEES3

--- seriália/beemooviescriptoverserial.py
+++ seriália/beemooviescriptoverserial.py
@@ -1,6 +1,6 @@
 import serial
 
-with open('beemoveiscript.txt', 'rb') as bmsf:
+with open('beemoveiscript2.txt', 'rb') as bmsf:
     bees = bmsf.read()
 com = serial.Serial('COM1', 9600)
 for bee in bees.splitlines():
