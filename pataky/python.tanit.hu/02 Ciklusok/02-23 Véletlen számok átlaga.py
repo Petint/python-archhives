5d17d5ef9c972698906c3833ec8ca5a364accbd4
Petrás Bálint <petras.balint04@gmail.com>
2023-01-20 23:28:33 +0100

    02-23 Véletlen számok átlaga

---
+++ pataky/python.tanit.hu/02 Ciklusok/02-23 Véletlen számok átlaga.py
@@ -0,0 +1,7 @@
+"""
+Generálj le 10 db véletlen kétjegyű egész számot.
+Írd ki a sorozatot emelkedő sorrendben.
+Számold ki az átlagukat kétféleképpen:
+a) a Python beépített függvényeit használva
+b) ciklust használva
+"""
