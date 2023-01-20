aa0673125ea00e4f7cdfb315bec355ea215a73ab
Petrás Bálint <petras.balint04@gmail.com>
2023-01-20 19:00:46 +0100

    02-16 Prímszámok

--- pataky/python.tanit.hu/02 Ciklusok/02-16 Prímszámok.py
+++ pataky/python.tanit.hu/02 Ciklusok/02-16 Prímszámok.py
@@ -2,3 +2,13 @@
 Olvassunk be egy egész számot és írjuk ki hogy prímszám-e.
 Abban biztosak lehetünk, hogy a felhasználó egynél nagyobb egész számot ad meg.
 """
+
+
+def is_prime():
+    print(number)
+    return False
+
+
+print('Egy beolvasott számról állapítjuk meg, hogy prím-e')
+number = int(input('Adj meg egy számot:'))
+print('A ', number, ' ' if is_prime() else ' nem ', 'prímszám', sep='')
