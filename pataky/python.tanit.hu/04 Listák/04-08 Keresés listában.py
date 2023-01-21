541d548dd55389041fd5e9bf09132ae09f10bcfb
Petrás Bálint <petras.balint04@gmail.com>
2023-01-21 19:17:24 +0100

    04-10 Számolás listában 04-08 Keresés listában

---
+++ pataky/python.tanit.hu/04 Listák/04-08 Keresés listában.py
@@ -0,0 +1,27 @@
+"""
+Ebből a listából programmal keresd ki a legidősebb budapestit és a minta szerint írd ki a nevét és életkorát.
+A program akkor is helyesen működjön, ha az adatlista módosul.
+"""
+dataList = """Név;Életkor;Város
+Németh Kamilla;19;Debrecen
+Fekete Géza;18;Pécs
+Kovács Péter;27;Budapest
+Kiss Tibor;20;Debrecen
+Szabó Erzsébet;21;Budapest
+Szilágyi Ede;18;Pécs
+Agárdi Pál;26;Budapest
+Pálosi Richárd;23;Budapest
+Budai Máté;19;Debrecen
+Karácsony Antal;20;Budapest
+Aradi Márta;27;Pécs
+Piros Adél;29;Debrecen
+Bíró Zsolt;16;Budapest
+Szabados Attila;25;Debrecen
+Román Sarolta;24;Budapest
+Virág Bertalan;22;Pécs
+Varga Imre;18;Budapest
+Tóth Sándor;22;Debrecen
+Nagy Ibolya;23;Pécs
+Horváth Ferenc;17;Budapest
+Balogh Edina;26;Budapest
+"""
---
+++ pataky/python.tanit.hu/04 Listák/04-10 Számolás listában.py
@@ -0,0 +1,27 @@
+"""
+Ebből a listából programmal számold meg hány 20 év feletti budapesti van.
+A program akkor is helyesen működjön, ha az adatlista módosul.
+"""
+dataList = """Név*Életkor*Város
+Nagy Ibolya*23*Pécs
+Horváth Ferenc*17*Budapest
+Agárdi Pál*26*Budapest
+Pálosi Richárd*23*Budapest
+Budai Máté*19*Debrecen
+Karácsony Antal*20*Budapest
+Román Sarolta*24*Budapest
+Virág Bertalan*22*Pécs
+Varga Imre*18*Budapest
+Kovács Péter*27*Budapest
+Kiss Tibor*20*Debrecen
+Szabó Erzsébet*21*Budapest
+Szilágyi Ede*18*Pécs
+Aradi Márta*27*Pécs
+Piros Adél*29*Debrecen
+Bíró Zsolt*16*Budapest
+Szabados Attila*25*Debrecen
+Tóth Sándor*22*Debrecen
+Balogh Edina*26*Budapest
+Németh Kamilla*19*Debrecen
+Fekete Géza*18*Pécs
+"""
