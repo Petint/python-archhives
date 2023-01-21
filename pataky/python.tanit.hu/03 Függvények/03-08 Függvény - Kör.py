70ba7c5f0f6d3428133775a55b7ceefb872d69a6
Petrás Bálint <petras.balint04@gmail.com>
2023-01-21 17:19:19 +0100

    03-08 Függvény - Kör Fekete Tamás köre

---
+++ pataky/python.tanit.hu/03 Függvények/03-08 Függvény - Kör.py
@@ -0,0 +1,29 @@
+"""
+Kérjük be a felhasználótól egy kör sugarát, cm-ben értve.
+Feltételezhetjük, hogy a felhasználó valóban számot ad meg, de az lehet tört szám is.
+Írj két saját függvényt a kör adatainak a kiszámításához.
+
+Az egyik függvény neve 'kerulet' legyen, bemenő paramétere a kör sugara cm-ben.
+A függvény számolja ki a kör kerületét és írja is ki az eredményt a konzolra a minta szerint, cm-ben.
+
+A másik függvény neve 'terulet' legyen, bemenő paramétere a kör sugara cm-ben.
+A függvény számolja ki a kör területét és adja vissza az eredményt.
+A szöveges, minta szerinti cm²-ben számolt kiírást a főprogramban hajtsuk végre.
+
+Az eredményeket mindkét esetben 2 tizedesre kerekítsük.
+"""
+from math import pi
+
+
+def kerulet(radius: float):
+    print('A kör kerülete:', round(2 * radius * pi, 2), 'cm')
+
+
+def felulet(r: float) -> float:
+    return (r ** 2) * pi
+
+
+print('Egy kor keruletet es feluletet fogjuk meghatarozni.')
+sugar = float(input('Add meg a kor sugarat: ').replace(',', '.'))
+kerulet(sugar)
+print('A kor felulete: %fcm2' % (felulet(sugar)))
