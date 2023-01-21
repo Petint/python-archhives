6731a2a43712726c35fc579fda0f3b7eaaa47e82
Petrás Bálint <petras.balint04@gmail.com>
2023-01-21 17:20:25 +0100

    03-09 Függvény - Négyzet

---
+++ pataky/python.tanit.hu/03 Függvények/03-09 Függvény - Négyzet.py
@@ -0,0 +1,14 @@
+"""
+Kérjük be a felhasználótól egy négyzet oldalhosszát, cm-ben értve.
+Feltételezhetjük, hogy a felhasználó valóban számot ad meg, de az lehet tört szám is.
+Írj két saját függvényt a négyzet adatainak a kiszámításához.
+
+Az egyik függvény neve 'kerulet' legyen, bemenő paramétere a négyzet oldala cm-ben.
+A függvény számolja ki a négyzet kerületét és írja is ki az eredményt a konzolra a minta szerint, cm-ben.
+
+A másik függvény neve 'terulet' legyen, bemenő paramétere a néygzet oldalhossza cm-ben.
+A függvény számolja ki a négyzet területét és adja vissza az eredményt.
+A szöveges, minta szerinti cm²-ben számolt kiírást a főprogramban hajtsuk végre.
+
+Az eredményeket mindkét esetben 2 tizedesre kerekítsük.
+"""
