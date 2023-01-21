306744dfa42b751d1f8b730f1ca7e0ac8e6df073
Petrás Bálint <petras.balint04@gmail.com>
2023-01-21 18:13:02 +0100

    03-10 Függvény Egyenlő oldalú háromszög

---
+++ pataky/python.tanit.hu/03 Függvények/03-10 Függvény - Egyenlő oldalú háromszög.py
@@ -0,0 +1,18 @@
+"""
+Feladat leírás:
+Kérjük be a felhasználótól egy egyenlő oldalú háromszög oldalhosszát, cm-ben értve.
+Feltételezhetjük, hogy a felhasználó valóban számot ad meg, de az lehet tört szám is.
+Írj két saját függvényt a egyenlő oldalú háromszög adatainak a kiszámításához.
+
+Az egyik függvény neve 'kerulet' legyen, bemenő paramétere a egyenlő oldalú háromszög oldala cm-ben.
+A függvény számolja ki a háromszög kerületét és írja is ki az eredményt a konzolra a minta szerint, cm-ben.
+
+A másik függvény neve 'terulet' legyen, bemenő paramétere a néygzet oldalhossza cm-ben.
+A függvény számolja ki a egyenlő oldalú háromszög területét és adja vissza az eredményt.
+A szöveges, minta szerinti cm²-ben számolt kiírást a főprogramban hajtsuk végre.
+
+Az eredményeket mindkét esetben 2 tizedesre kerekítsük.
+"""
+
+print('Egy egyenlő oldalú háromszög oldalhosszát kérjük be\nés kiírjuk a kerületét cm-ben és a területét cm²-ben.')
+
