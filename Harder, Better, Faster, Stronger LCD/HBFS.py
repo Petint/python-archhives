42ec3e4bd9014725f4bb70e3ebd6a3e9c81c94ae
Petrás Bálint <petras.balint04@gmail.com>
2023-03-02 17:57:21 +0100

    timing

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -7,10 +7,12 @@ def main():
     lirics_file = open("lirics.txt", 'rt')
     for line in lirics_file.read().splitlines():
         if len(line) > 16:
-            line = line[0:15] + "123456789" + line[16:]
-        tty.write(bytes(line))
+            line = line[0:16] + "123456789123456789123567" + line[16:]
+        print(line)
+        tty.write(bytes(line, encoding="UTF-8"))
         time.sleep(1)
     lirics_file.close()
+    tty.close()
 
 if __name__ == "__main__":
     main()
