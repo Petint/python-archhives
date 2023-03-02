99d7f30075da1898588f407152a81d27c9aa1bb2
Petrás Bálint <petras.balint04@gmail.com>
2023-03-02 18:18:39 +0100

    improver timing

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -6,7 +6,7 @@ def main():
     tty = serial.Serial('COM5',9600)
     lirics_file = open("lirics.txt", 'rt')
     for line in lirics_file.read().splitlines():
-        line, line_time = line.split(',')
+        line, line_time = line.split('??')
         if len(line) > 16:
             line = line[0:16] + "123456789123456789123567" + line[16:]
         print(line)
