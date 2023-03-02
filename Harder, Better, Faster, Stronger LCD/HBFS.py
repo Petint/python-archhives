8dda0d0fc6a8242d025d6dccc896be44948efe37
Petrás Bálint <petras.balint04@gmail.com>
2023-03-02 17:44:20 +0100

    cvcc

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -5,8 +5,10 @@ import serial
 def main():
     tty = serial.Serial('COM5',9600)
     lirics_file = open("lirics.txt", 'rt')
-    for line in lirics_file:
-        tty.write(bytes(line.strip(b'\n')))
+    for line in lirics_file.read().splitlines():
+        if len(line) > 16:
+            line = line[0:15] + "123456789" + line[16:]
+        tty.write(bytes(line))
         time.sleep(1)
     lirics_file.close()
 
