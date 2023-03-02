7a5e14eafe25c5a0d0eb61a27d122c361b928f3b
Petrás Bálint <petras.balint04@gmail.com>
2023-03-02 17:37:43 +0100

    fc

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -6,7 +6,7 @@ def main():
     tty = serial.Serial('COM5',9600)
     lirics_file = open("lirics.txt", 'rt')
     for line in lirics_file:
-        tty.write(line.strip('\n'))
+        tty.write(bytes(line.strip(b'\n')))
         time.sleep(1)
     lirics_file.close()
 
