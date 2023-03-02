72ac6c8a16848953523dfe74b25d461597bf37c6
Petrás Bálint <petras.balint04@gmail.com>
2023-03-02 17:30:30 +0100

    ff

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -1,10 +1,14 @@
 import time
-import Sereal
+# import Serial
 
 
 def main():
-    pass
-
+    # tty = Serial.Serial('COM5',9600)
+    lirics_file = open("lirics.txt", 'rt')
+    for line in lirics_file:
+        print(line.strip('\n'))
+        time.sleep(1)
+    lirics_file.close()
 
 if __name__ == "__main__":
     main()
