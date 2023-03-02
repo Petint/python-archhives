391d769815bd645f498eb9f251aa934ee5bcc17b
Petrás Bálint <petras.balint04@gmail.com>
2023-03-02 18:06:44 +0100

    I implemented somethink known as "timing"

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -6,11 +6,12 @@ def main():
     tty = serial.Serial('COM5',9600)
     lirics_file = open("lirics.txt", 'rt')
     for line in lirics_file.read().splitlines():
+        line, line_time = line.split(',')
         if len(line) > 16:
             line = line[0:16] + "123456789123456789123567" + line[16:]
         print(line)
         tty.write(bytes(line, encoding="UTF-8"))
-        time.sleep(1)
+        time.sleep(int(line_time))
     lirics_file.close()
     tty.close()
 
