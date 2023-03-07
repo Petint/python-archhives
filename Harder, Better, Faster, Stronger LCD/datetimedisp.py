ef768370145380c1c719aedde0159ba0be353bb4
Petrás Bálint <petras.balint04@gmail.com>
2023-03-07 15:57:55 +0100

    jjj

--- Harder, Better, Faster, Stronger LCD/datetimedisp.py
+++ Harder, Better, Faster, Stronger LCD/datetimedisp.py
@@ -4,13 +4,11 @@ import serial
 
 def main():
     tty = serial.Serial('COM5', 9600)
-    tty.write(b'0') # datetime.datetime.now().strftime("%Y-%M-%D %H:%M")
-    screen = "2023-01-01 00:00"
-    tty.write(bytes(screen, encoding="utf-8"))
+    screen = datetime.datetime.now().strftime("%Y-%m-%d %H:%M").encode()
+    tty.write(screen)
     print(screen)
     tty.close()
 
 
 if __name__ == '__main__':
-    while True:
-     main()
+    main()
