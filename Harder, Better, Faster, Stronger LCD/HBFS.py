8dd6236c66a0fc78ddd5acb93d67c1dc398fe1ca
Petrás Bálint <petras.balint04@gmail.com>
2023-11-13 00:04:37 +0100

    display test computer software programme

--- Harder, Better, Faster, Stronger LCD/HBFS.py
+++ Harder, Better, Faster, Stronger LCD/HBFS.py
@@ -4,13 +4,14 @@ import serial
 
 def main():
     tty = serial.Serial('COM1',9600)
+    tty.write(bytes('<ID01>  \r\n', encoding="UTF-8"))
     lirics_file = open("lirics.txt", 'rt')
     for line in lirics_file.read().splitlines():
         line, line_time = line.split('??')
-        if len(line) > 16:
-            line = line[0:16] + "123456789123456789123567" + line[16:]
+        # if len(line) > 16:
+        #     line = line[0:16] + "123456789123456789123567" + line[16:]
         print(line)
-        tty.write(bytes(line, encoding="UTF-8"))
+        tty.write(bytes('<ID01>'+line, encoding="UTF-8"))
         time.sleep(int(line_time))
     lirics_file.close()
     tty.close()
--- Harder, Better, Faster, Stronger LCD/datetimedisp.py
+++ Harder, Better, Faster, Stronger LCD/datetimedisp.py
@@ -3,8 +3,9 @@ import serial
 
 
 def main():
-    tty = serial.Serial('COM5', 9600)
-    screen = datetime.datetime.now().strftime("%Y-%m-%d %H:%M").encode()
+    tty = serial.Serial('COM1', 9600)
+    tty.write(b'<ID01>')
+    screen = b'<ID01>'+datetime.datetime.now().strftime("%Y-%m-%d %H:%M").encode()
     tty.write(screen)
     print(screen)
     tty.close()
